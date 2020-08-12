ad27766f6f0ab0f39de7f4a6e74a659f182cd968
Michael Dotson <thedotson@icloud.com>
2020-08-11 20:56:42 -0500

    setup shape props student data and baseline for exported functions

---
+++ src/helpers/data/studentData.js
@@ -0,0 +1,157 @@
+const students = [
+  {
+    id: 'student01',
+    firstName: 'Austin',
+    lastName: 'Phy',
+    isDead: false,
+  },
+  {
+    id: 'student02',
+    firstName: 'Brooke',
+    lastName: 'Nemchak',
+    isDead: false,
+  },
+  {
+    id: 'student03',
+    firstName: 'Gene',
+    lastName: 'McNew',
+    isDead: false,
+  },
+  {
+    id: 'student04',
+    firstName: 'Gwynne',
+    lastName: 'Meeks',
+    isDead: false,
+  },
+  {
+    id: 'student05',
+    firstName: 'Jeanine',
+    lastName: 'Beckle',
+    isDead: false,
+  },
+  {
+    id: 'student06',
+    firstName: 'Jeremiah',
+    lastName: 'Vasquez',
+    isDead: false,
+  },
+  {
+    id: 'student07',
+    firstName: 'Jim',
+    lastName: 'Browning',
+    isDead: false,
+  },
+  {
+    id: 'student08',
+    firstName: 'Jonathan',
+    lastName: 'Shearon',
+    isDead: false,
+  },
+  {
+    id: 'student09',
+    firstName: 'Joshua',
+    lastName: 'Medlen',
+    isDead: false,
+  },
+  {
+    id: 'student10',
+    firstName: 'Kamiran',
+    lastName: 'Ibrahim',
+    isDead: false,
+  },
+  {
+    id: 'student11',
+    firstName: 'Mark',
+    lastName: 'Young',
+    isDead: false,
+  },
+  {
+    id: 'student12',
+    firstName: 'Matt',
+    lastName: 'Logan',
+    isDead: false,
+  },
+  {
+    id: 'student13',
+    firstName: 'Michael',
+    lastName: 'Dotson',
+    isDead: false,
+  },
+  {
+    id: 'student14',
+    firstName: 'Nate',
+    lastName: 'Owens',
+    isDead: false,
+  },
+  {
+    id: 'student15',
+    firstName: 'Nick',
+    lastName: 'Walters',
+    isDead: false,
+  },
+  {
+    id: 'student16',
+    firstName: 'Nikhil',
+    lastName: 'Gaikwad',
+    isDead: false,
+  },
+  {
+    id: 'student17',
+    firstName: 'Ola',
+    lastName: 'Oladinni',
+    isDead: false,
+  },
+  {
+    id: 'student18',
+    firstName: 'Pete',
+    lastName: 'Stewart',
+    isDead: false,
+  },
+  {
+    id: 'student19',
+    firstName: 'Phonesalo',
+    lastName: 'Sihanorak',
+    isDead: false,
+  },
+  {
+    id: 'student20',
+    firstName: 'Ryan',
+    lastName: 'Beiden',
+    isDead: false,
+  },
+  {
+    id: 'student21',
+    firstName: 'William',
+    lastName: 'Campbell',
+    isDead: false,
+  },
+];
+
+const getStudents = () => students;
+
+// const livingStudents = () => {
+//   if (students.isDead === false) {
+//     return students;
+//   }
+// };
+
+// const dearlyBeloved = () => {
+//   if (students.isDead === true) {
+//     return students;
+//   }
+// };
+
+const followTheLight = (studentId) => {
+  students.forEach((response) => {
+    if (response.id === studentId) {
+      response.isDead = true;
+    }
+  });
+};
+
+export default {
+  getStudents,
+  // livingStudents,
+  // dearlyBeloved,
+  followTheLight,
+};
